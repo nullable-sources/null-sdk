6bb27d3fabf8db65132da1054b143dc6d509b6eb
suddenly <suddenlybydl0@gmail.com>
2022-05-24 08:42:01 +0500

    Minor changes.

--- src/data-types/rect.h
+++ src/data-types/rect.h
@@ -9,8 +9,8 @@ struct rect_t {
 	template <typename t1, typename t2> rect_t(t1 _min, t2 _max) : min(_min), max(_max) { }
 	template <typename t1, typename t2, typename t3, typename t4> rect_t(t1 min_x, t2 min_y, t3 max_x, t4 max_y) : min(min_x, min_y), max(max_x, max_y) { }
 
-	vec2_t size() { return max - min; }
-	vec2_t center() { return (min + max) / 2.f; }
+	vec2_t size() const { return max - min; }
+	vec2_t center() const { return (min + max) / 2.f; }
 
 	class_create_operators(rect_t, -, { return rect_t(-min, -max); });
 	class_create_arithmetic_operators(rect_t, +, { return rect_t(min + a.min, max + a.max); }, { min += a.min; max += a.max; return *this; });
