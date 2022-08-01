ca559d4dc20e290de9770920e1c4566f8e73b4ae
suddenly <suddenlybydl0@gmail.com>
2022-08-02 03:44:38 +0500

    🥴

--- src/data-types/rect.h
+++ src/data-types/rect.h
@@ -9,8 +9,9 @@ struct rect_t {
 	rect_t(auto _min, auto _max) : min(_min), max(_max) { }
 	rect_t(auto min_x, auto min_y, auto max_x, auto max_y) : min(min_x, min_y), max(max_x, max_y) { }
 
-	bool contains(const vec2_t& a) const { return min <= a && max >= a; }
+	bool contains(const vec2_t& point) const { return min <= point && max >= point; }
 	bool contains(const rect_t& rect) const { return contains(rect.min) || contains(rect.max); }
+	bool intersects(const rect_t& rect) const { return !((rect.max.y <= min.y || rect.max.x <= min.x) || (rect.min.y >= max.y || rect.min.x >= max.y)); }
 
 	vec2_t size() const { return max - min; }
 	vec2_t center() const { return (min + max) / 2.f; }
