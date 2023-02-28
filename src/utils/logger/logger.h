c470e6577d94c797836d5e20ae46f7e99609670a
suddenly <suddenlybydl0@gmail.com>
2023-02-28 16:05:16 +0500

    Added assert log type.

--- src/utils/logger/logger.h
+++ src/utils/logger/logger.h
@@ -4,16 +4,17 @@
 #include <utils/events/events.h>
 
 namespace utils {
-    enum class e_log_type { info, warning, error };
+    enum class e_log_type { info, warning, error, assert };
     class c_log_dispatcher : public c_event_dispatcher<e_log_type> {
     public:
         class c_default_log_listener : public i_event_listener<e_log_type> {
         public:
             void process_event(const e_log_type& id, const std::unordered_map<std::string, std::any>& parameters) override {
                 switch(id) {
-                    case e_log_type::info: { std::cout << "\033[3;104;30m" << "[info]" << "\033[0m " << "\x1B[36m" << std::any_cast<std::string>(parameters.at("text")) << "\033[0m" << std::endl; } break;
-                    case e_log_type::warning: { std::cout << "\033[3;43;30m" << "[warning]" << "\033[0m " << "\x1B[33m" << std::any_cast<std::string>(parameters.at("text")) << "\033[0m" << std::endl; } break;
-                    case e_log_type::error: { throw std::runtime_error{ std::format("\033[3;42;30m[error]\033[0m \x1B[31m{}\033[0m\n", std::any_cast<std::string>(parameters.at("text"))) }; } break;
+                    case e_log_type::info:      { std::cout << "\033[3;104;30m[info]\033[0m \x1B[36m"   << std::any_cast<std::string>(parameters.at("text")) << "\033[0m" << std::endl; } break;
+                    case e_log_type::warning:   { std::cout << "\033[3;43;30m[warning]\033[0m \x1B[33m" << std::any_cast<std::string>(parameters.at("text")) << "\033[0m" << std::endl; } break;
+                    case e_log_type::error:     { std::cout << "\033[3;42;30m[error]\033[0m \x1B[31m"   << std::any_cast<std::string>(parameters.at("text")) << "\033[0m" << std::endl; } break;
+                    case e_log_type::assert:    { throw std::runtime_error{ std::format("\033[3;42;30m[assert]\033[0m \x1B[31m{}\033[0m\n", std::any_cast<std::string>(parameters.at("text"))) }; } break;
                 }
             }
         } default_listener{ };
@@ -23,6 +24,7 @@ namespace utils {
             attach_listener(e_log_type::info, &default_listener);
             attach_listener(e_log_type::warning, &default_listener);
             attach_listener(e_log_type::error, &default_listener);
+            attach_listener(e_log_type::assert, &default_listener);
         }
 
     public:
