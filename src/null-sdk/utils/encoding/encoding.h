8534e30c2d87e319082944c163606926374fd22f
suddenly <suddenlybydl0@gmail.com>
2024-05-05 17:43:17 +0500

    Added new encoding methods and renamed old.

--- src/null-sdk/utils/encoding/encoding.h
+++ src/null-sdk/utils/encoding/encoding.h
@@ -3,15 +3,21 @@
 #include "base85.h"
 
 namespace utils::encoding {
-	static std::string utf8_to_ascii(std::wstring_view utf8) {
-		std::string ascii(WideCharToMultiByte(CP_UTF8, 0, utf8.data(), utf8.size(), 0, 0, 0, 0), '\0');
-		WideCharToMultiByte(CP_UTF8, 0, utf8.data(), utf8.size(), ascii.data(), ascii.length(), 0, 0);
+	static std::string utf16_to_ascii(std::wstring_view utf16) {
+		std::string ascii(WideCharToMultiByte(CP_ACP, 0, utf16.data(), utf16.size(), 0, 0, 0, 0), '\0');
+		WideCharToMultiByte(CP_ACP, 0, utf16.data(), utf16.size(), ascii.data(), ascii.length(), 0, 0);
 		return ascii;
 	}
 
-	static std::wstring ascii_to_utf8(std::string_view ascii) {
-		std::wstring utf8(MultiByteToWideChar(CP_ACP, 0, ascii.data(), ascii.length(), 0, 0), L'\0');
-		MultiByteToWideChar(CP_ACP, 0, ascii.data(), ascii.length(), utf8.data(), utf8.length());
+	static std::wstring ascii_to_utf16(std::string_view ascii) {
+		std::wstring utf16(MultiByteToWideChar(CP_ACP, 0, ascii.data(), ascii.length(), 0, 0), L'\0');
+		MultiByteToWideChar(CP_ACP, 0, ascii.data(), ascii.length(), utf16.data(), utf16.length());
+		return utf16;
+	}
+
+	static std::string utf16_to_utf8(std::wstring_view utf16) {
+		std::string utf8(WideCharToMultiByte(CP_UTF8, 0, utf16.data(), utf16.size(), 0, 0, 0, 0), '\0');
+		WideCharToMultiByte(CP_UTF8, 0, utf16.data(), utf16.size(), utf8.data(), utf8.length(), 0, 0);
 		return utf8;
 	}
 
