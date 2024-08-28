a51cf6274aff3628773905f35d638239eb0783ae
suddenly <suddenlybydl0@gmail.com>
2024-08-29 04:43:09 +0500

    Added memory::pe_image_t::cast.

--- src/null-sdk/utils/memory/pe-image.h
+++ src/null-sdk/utils/memory/pe-image.h
@@ -13,6 +13,9 @@ namespace memory {
         inline constexpr pe_image_t(value_t value) : base_address(value) { }
 
     public:
+        template <typename cast_t> inline constexpr cast_t cast() const { return base_address.cast<cast_t>(); }
+
+    public:
         inline constexpr PIMAGE_DOS_HEADER dos_header() { return base_address.cast<PIMAGE_DOS_HEADER>(); }
         inline PIMAGE_NT_HEADERS nt_headers() { return base_address.offseted(dos_header()->e_lfanew).cast<PIMAGE_NT_HEADERS>(); }
 
